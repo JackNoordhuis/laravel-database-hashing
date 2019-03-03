4d8916553b1146f3675a4e483b7f415abbef0a95
Jack Noordhuis <daswaggamer@gmail.com>
2019-03-03 16:05:06 +1100

    Add tests for hash creations

--- tests/hashing/HashingHelperTest.php
+++ tests/hashing/HashingHelperTest.php
@@ -56,4 +56,63 @@ class HashingHelperTest extends TestCase
         $this->assertFalse($helper->enabled());
         $this->assertNull($helper->salt());
     }
+
+    /**
+     * Make sure the same application salt and input value create the same hash.
+     */
+    public function test_same_app_salt_hash(): void
+    {
+        $helper = new HashingHelper(true, 'an application salt');
+        $hash_1 = $helper->create('some random data');
+        $hash_2 = $helper->create('some random data');
+
+        $this->assertEquals($hash_1, $hash_2);
+    }
+
+    /**
+     * Make sure two different application salts create different hashes on the same data.
+     */
+    public function test_different_app_salt_hash(): void
+    {
+        $hash_1 = (new HashingHelper(true, 'an application salt'))->create('some random data');
+        $hash_2 = (new HashingHelper(true, 'another application salt'))->create('some random data');
+
+        $this->assertNotEquals($hash_1, $hash_2);
+    }
+
+    /**
+     * Make sure the same application salt creates a different hash for each input value.
+     */
+    public function test_different_value(): void
+    {
+        $helper = new HashingHelper(true, 'an application salt');
+        $hash_1 = $helper->create('some random data');
+        $hash_2 = $helper->create('some more random data');
+
+        $this->assertNotEquals($hash_1, $hash_2);
+    }
+
+    /**
+     * Make sure the same application salt, input values and salt modifiers create the same hash.
+     */
+    public function test_same_salt_modifier(): void
+    {
+        $helper = new HashingHelper(true, 'an application salt');
+        $hash_1 = $helper->create('some random data', 'a custom salt modifier');
+        $hash_2 = $helper->create('some random data', 'a custom salt modifier');
+
+        $this->assertEquals($hash_1, $hash_2);
+    }
+
+    /**
+     * Make sure the same application salt and input values with different salt modifiers creates different hashes.
+     */
+    public function test_different_salt_modifier(): void
+    {
+        $helper = new HashingHelper(true, 'an application salt');
+        $hash_1 = $helper->create('some random data', 'a custom salt modifier');
+        $hash_2 = $helper->create('some random data', 'another custom salt modifier');
+
+        $this->assertNotEquals($hash_1, $hash_2);
+    }
 }
